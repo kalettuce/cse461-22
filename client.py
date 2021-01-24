560512d25cf83148deaa6e7bee13878794280823
unknown <nousernameavailablexcepthisone@gmail.com>
2021-01-23 16:10:50 -0800

    makepacket()

--- client.py
+++ client.py
@@ -1,29 +1,20 @@
 from socket import *
-import sys
+import struct
 
 attu2 = '128.208.1.138'
 attu3 = '128.208.1.139'
 port = 12235
+ID = 423
 
-message = bytearray()
-message.append(0x00)
-message.append(0x00)
-message.append(0x00)
-message.append(0x0C)
-message.append(0x00)
-message.append(0x00)
-message.append(0x00)
-message.append(0x00)
-message.append(0x00)
-message.append(0x01)
-message.append(0x01)
-message.append(0xA7)
-payload = bytes("hello world", 'utf-8')
-message.extend(payload)
-message.append(0x00)
-print(message)
+def makepacket(length, step, psec, message):
+    header = struct.pack('>iihh', length, step, psec, ID)
+    return header + message.encode('utf-8') + (4 - len(message) % 4) * b'\x00'
+
+packet = makepacket(12, 1, 0, "hello world")
+print(packet)
+
 s = socket(AF_INET, SOCK_DGRAM)
-s.sendto(message, (attu2, port))
-data, server = s.recvfrom(4096)
+s.sendto(packet, (attu2, port))
+data = s.recv(4096)
 print(data)
 print("done")
